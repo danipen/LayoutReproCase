11d719f5b1dc6a9b1212f466afa2f8536ab59456
Daniel <dpenalba@codicesoftware.com>
2023-04-14 14:27:02 +0200

    Add project files.

---
+++ LayoutReproCase/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace LayoutReproCase
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
